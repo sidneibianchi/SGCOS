6695e0003acfe9a2e865c9e8c7ac314ae8bd29e8
sidneibianchi <sidneibianchi007@hotmail.com>
2019-05-23 19:30:31 -0300

    edit chamado

---
+++ SGCOS-App/src/app/chamados/chamado-edit/chamado-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-edit',
+  templateUrl: './chamado-edit.component.html',
+  styleUrls: ['./chamado-edit.component.css']
+})
+export class ChamadoEditComponent implements OnInit {
+
+  titulo = 'Edição de Chamados';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
