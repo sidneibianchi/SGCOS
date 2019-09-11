0dcdf5884f2f67696d7ddddbbdecd5cf9fd10a93
sidneibianchi <sidneibianchi007@hotmail.com>
2019-09-11 20:34:19 -0300

    img

--- SGCOS.Domain/Equipamento.cs
+++ SGCOS.Domain/Equipamento.cs
@@ -10,6 +10,7 @@ namespace SGCOS.Domain
         public string Descricao { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
+        public string img { get; set; }
         public int ClienteId { get; set; }
         public Cliente Cliente { get; }
         public List<Servico> Servicos { get; set; }
--- SGCOS.WebAPI/Controllers/EquipamentoController.cs
+++ SGCOS.WebAPI/Controllers/EquipamentoController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -174,6 +176,35 @@ namespace SGCOS.WebAPI.Controllers
             return BadRequest();
         }
 
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
+                    var fullPath = Path.Combine(pathToSave, filename.Replace("\"", " ").Trim());
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
+            }
+
+            return BadRequest("Erro ao tentar realizar upload");
+        }
 
     }
 }
